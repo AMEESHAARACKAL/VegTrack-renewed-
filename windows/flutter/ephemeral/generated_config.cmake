a04fab6ea6991b7182b7ea26ac2a5b366974ca67
Ameesha Arackal <75663248+AMEESHAARACKAL@users.noreply.github.com>
2024-05-04 23:25:26 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\amees\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\amees\\Downloads\\freshness-main\\freshness-main" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\amees\\flutter"
+  "PROJECT_DIR=C:\\Users\\amees\\Downloads\\freshness-main\\freshness-main"
+  "FLUTTER_ROOT=C:\\Users\\amees\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\amees\\Downloads\\freshness-main\\freshness-main\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\amees\\Downloads\\freshness-main\\freshness-main"
+  "FLUTTER_TARGET=C:\\Users\\amees\\Downloads\\freshness-main\\freshness-main\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jNGNkNDhlMTg2NDYwYjMyZDQ0NTg1Y2UzYzEwMzI3MWFiNjc2MzU1Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\amees\\Downloads\\freshness-main\\freshness-main\\.dart_tool\\package_config.json"
+)
